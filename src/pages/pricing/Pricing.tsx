fcd2eb955ee0fb724ac6e8aafb9de3e65b0eb6b8
Ashish Makwana <85098548+ashishmakwana96am@users.noreply.github.com>
2025-07-11 17:08:35 +0530

    changes

--- src/pages/pricing/Pricing.tsx
+++ src/pages/pricing/Pricing.tsx
@@ -9,7 +9,7 @@ function Pricing() {
                 <div className="border-b border-2 border-[#212121]/10">
                         <h3 className="text-[#202224] text-[22px] font-bold mb-[10px]">Basic</h3>
                     <h6 className="text-[#212121]">Monthly Charge</h6>
-                    <h2>$14.99</h2>
+                    <h2>$14.99</h2> 
                 </div>
             </div>
        </div>
